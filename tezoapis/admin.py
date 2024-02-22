dddda8c55d6ce9083702a4a06794a4be8cc77ec4
githubaccount-2022 <shravanjerry2001@gmail.com>
2024-02-23 02:40:23 +0530

    Update admin.py

--- tezoapis/admin.py
+++ tezoapis/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+from .models import Employee
 
-# Register your models here.
+class EmployeeAdmin(ImportExportModelAdmin):
+    list_display = ('empid', 'firstname', 'lastname', 'DOB', 'emailid', 'mobile', 'joining', 'location', 'jobtitle', 'Department', 'AssignManager', 'AssignProject', 'profilepath', 'status')
+    list_filter = ('location', 'jobtitle', 'Department', 'status')
+    search_fields = ('empid', 'firstname', 'lastname', 'emailid', 'mobile')
+
+admin.site.register(Employee, EmployeeAdmin)
